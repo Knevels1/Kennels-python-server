9347641d8e7c74a6788010131cf6824014913bdd
Knevels1 <kyle.a.nevels@icloud.com>
2021-01-22 10:15:16 -0600

    allowed get method for animals, customers, locations, employees

--- customers/request.py
+++ customers/request.py
@@ -1,73 +1,67 @@
-CUSTOMERS = [
-    {
-      "id": 1,
-      "email": "DawnOps@icloud.com",
-      "password": "19972011",
-      "name": "Kyle Nevels"
-    },
-    {
-      "id": 2,
-      "email": "mauligan@hghg.com",
-      "password": "13792",
-      "name": "Kyle Nevels"
-    },
-    {
-      "id": 3,
-      "email": "mcrib@gmail.com",
-      "password": "143",
-      "name": "Dawny Nevels",
-    }
-]
+import sqlite3
+import json
+from models import Customer
+
 def get_all_customers():
-    return CUSTOMERS
-    # Function with a single parameter
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.password,
+            c.email
+        FROM customer c
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            customer = Customer(row['id'], row['name'], row['password'],
+                            row['email'])
+
+            customers.append(customer.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(customers)
 def get_single_customer(id):
-    # Variable to hold the found customer, if it exists
-    requested_customer = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the Customers list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for customer in CUSTOMERS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if customer["id"] == id:
-            requested_customer = customer
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.password,
+            c.email
+        FROM customer c
+        WHERE c.id = ?
+        """, ( id, ))
 
-    return requested_customer
-def create_customers(customer):
-    # Get the id value of the last customer in the list
-    max_id = CUSTOMERS[-1]["id"]
+        # Load the single result into memory
+        data = db_cursor.fetchone()
 
-    # Add 1 to whatever that number is
-    new_id = max_id + 1
+        # Create an animal instance from the current row
+        customer = Customer(data['id'], data['name'], data['password'],
+                            data['email'])
 
-    # Add an `id` property to the customer dictionary
-    customer["id"] = new_id
-
-    # Add the customer dictionary to the list
-    CUSTOMERS.append(customer)
-
-    # Return the dictionary with `id` property added
-    return customer
-def delete_customer(id):
-    # Initial -1 value for customer index, in case one isn't found
-    customer_index = -1
-
-    # Iterate the Customers list, but use enumerate() so that you
-    # can access the index value of each item
-    for index, customer in enumerate(CUSTOMERS):
-        if customer["id"] == id:
-            # Found the customer. Store the current index.
-            customer_index = index
-
-    # If the customer was found, use pop(int) to remove it from list
-    if customer_index >= 0:
-        CUSTOMERS.pop(customer_index)
-def update_customer(id, new_customer):
-    # Iterate the CUSTOMER list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, customer in enumerate(CUSTOMERS):
-        if customer["id"] == id:
-            # Found the customer. Update the value.
-            CUSTOMERS[index] = new_customer
-            break
+        return json.dumps(customer.__dict__)
