31048915586f6061d890c02d9d798e948d64c7f5
Knevels1 <kyle.a.nevels@icloud.com>
2021-01-24 18:56:40 -0600

    put in the function so that you can get a customer by their email

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal
-from customers import get_all_customers, get_single_customer
+from customers import get_all_customers, get_single_customer, get_customers_by_email
 from employees import get_all_employees, get_single_employee
 from locations import get_all_locations, get_single_location
 
@@ -10,7 +10,7 @@ from locations import get_all_locations, get_single_location
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
-      def parse_url(self, path):
+    def parse_url(self, path):
         path_params = path.split("/")
         resource = path_params[1]
 
