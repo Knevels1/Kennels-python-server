9db61c7981929f0ec0debb102556d7a8e685a831
Knevels1 <kyle.a.nevels@icloud.com>
2021-01-24 19:24:43 -0600

    added the function so that an animal can be deleted

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, get_animals_by_location, get_animals_by_status
+from animals import delete_animal
 from customers import get_all_customers, get_single_customer, get_customers_by_email
 from employees import get_all_employees, get_single_employee, get_employees_by_location
 from locations import get_all_locations, get_single_location
